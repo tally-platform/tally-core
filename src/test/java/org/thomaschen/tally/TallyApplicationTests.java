7e5c845a0485312478a0ce9cfdc2e6f91a1b27fe
Thomas <thomas99k@gmail.com>
2019-01-28 17:49:36 -0500

    core - git add working integration test

--- src/test/java/org/thomaschen/tally/TallyApplicationTests.java
+++ src/test/java/org/thomaschen/tally/TallyApplicationTests.java
@@ -1,19 +1,43 @@
 package org.thomaschen.tally;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MySQLContainer;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = TallyApplicationTests.Initializer.class)
 public class TallyApplicationTests {
+
+    @ClassRule
+    public static MySQLContainer mySQLContainer = new MySQLContainer()
+            .withUsername("inmnem")
+            .withPassword("inmem");
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues values = TestPropertyValues.of(
+                    "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + mySQLContainer.getUsername(),
+                    "spring.datasource.password=" + mySQLContainer.getPassword()
+            );
+        }
+    }
 
     @Test
     public void contextLoads() {
 
     }
+
 
 }
