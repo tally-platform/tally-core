601f9fcb35d4d778d89f3cfb3f7761e6b0d4523e
Thomas <thomas99k@gmail.com>
2019-02-15 16:21:53 -0500

    core - add Application class

--- src/main/java/org/thomaschen/tally/TallyApplication.java
+++ src/main/java/org/thomaschen/tally/TallyApplication.java
@@ -4,16 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.security.Principal;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 @RestController
 @PropertySource("database.properties")
 public class TallyApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TallyApplication.class, args);
